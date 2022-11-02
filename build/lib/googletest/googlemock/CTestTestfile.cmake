c1d6d219f4311f84e8438b58d771f2b96313bf67
Dennis <wennisdagner@yahoo.de>
2022-11-02 18:35:56 +0100

    fix structure

--- build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dennis/.jenkins/workspace/CppPipeline_main/lib/googletest/googlemock
-# Build directory: /home/dennis/.jenkins/workspace/CppPipeline_main/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dennis/.jenkins/workspace/CppPipeline_main/lib/googletest/googletest
-# Build directory: /home/dennis/.jenkins/workspace/CppPipeline_main/build/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
