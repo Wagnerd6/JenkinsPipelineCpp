cc320674c36e4687ea1cabe10113714fcc35b40f
Wagnerd6 <wennisdagner@yahoo.de>
2022-11-14 17:38:49 +0100

    Debug: Print env and GIT_URL

--- vars/startPipeline.groovy
+++ vars/startPipeline.groovy
@@ -2,4 +2,6 @@ def call(String configJsonPath = "PipelineConfig.json", def body) {
     println("Debug: startPipeline executed")
     def json = readJSON(file: configJsonPath, returnPojo: true)
     println("Try printing json: " + json)
+    sh "env"
+    echo "GIT URL $GIT_URL"
 }
